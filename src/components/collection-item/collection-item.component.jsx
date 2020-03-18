fe987d26de500724e4ec9f4f59ef438c00d2fcde
Hanut Singh Gusain <hanutsingh@gmail.com>
2020-03-18 14:26:21 +0530

    Added button for adding items to cart in the shop page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,20 +5,22 @@ import "./style.scss";
 
 const CollectionItem = ({ id, name, price, image }) => (
   <div className="collection-item" title={name + " @ Rs. " + price}>
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${image})`
-      }}
-      id={id}
-    />
-    <div className="collection-footer" >
+    <div className="image-holder">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${image})`
+        }}
+        id={id}
+      />
+    </div>
+    <div className="collection-footer">
       <span className="name">
         {name.length > 25 ? name.substr(0, 25) + "..." : name}
       </span>
       <span className="price">&#8377;{price}</span>
     </div>
-    <CustomButton>Add to Cart</CustomButton>
+    <CustomButton inverted={true}>Add to Cart</CustomButton>
   </div>
 );
 
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./style.scss";
 
 const CustomButton = ({ children, className, inverted, ...otherProps }) => (
-  <button className={`${className ? className : ''} custom-button${inverted ? 'inverted':''}`} {...otherProps}>
+  <button className={`${className ? className : ''} custom-button${inverted ? ' inverted':''}`} {...otherProps}>
     {children}
   </button>
 );
