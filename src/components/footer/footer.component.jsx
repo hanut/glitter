6e5a83eb1faa9a90d2841bef768459c09af6f23c
Hanut Singh Gusain <hanutsingh@gmail.com>
2020-04-06 14:21:47 +0530

    Updated the footer link
    :

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 import './styles.scss';
 
 const FooterComponent = () => (
   <footer className="footer">
     &copy;2020&nbsp;&nbsp;
-    <Link to="https://hanutsingh.in">Hanut Singh Gusain</Link>
+    <a target="blank" href="https://hanutsingh.in">Hanut Singh Gusain</a>
   </footer>
 )
 
